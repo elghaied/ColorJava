40f4b3e3eada3febc1178f3d5d8ea83ddf479024
elghaied <elghaied.eslam@gmail.com>
2021-03-29 15:25:43 +0200

    creating classes

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import color.model.Color;
+
+public class Main {
+    public static void main(String[] args) {
+        Color test = new Color("#regrbl");
+        System.out.println(test.toString());
+
+        Color test2 = new Color(0,355,70);
+        System.out.println(test2);
+    }
+
+}
--- src/color/model/Color.java
+++ src/color/model/Color.java
@@ -1,33 +1,77 @@
 package color.model;
 
+import java.util.Arrays;
+
 public class Color {
     int red;
     int green;
     int blue;
     String HexValue;
 
-    public Color(String hexValue)  throws IllegalArgumentException  {
-        HexValue = hexValue;
+
+
+    public Color(String hexValue) throws IllegalArgumentException {
+        this.setHexValue(hexValue);
     }
 
-    public Color(int red, int green, int blue) throws IllegalArgumentException  {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+    public Color(int red, int green, int blue) throws IllegalArgumentException {
+        this.setRed(red);
+        this.setGreen(green);
+        this.setBlue(blue);
     }
 
+    /**
+     * Setters
+     *
+     */
     public void setRed(int red) {
-        this.red = red;
+        if(red >= 0 && red <= 255){
+            this.red = red;
+        }else {
+            throw new IllegalArgumentException("Put a value between 0 and 255, you have assigned for red :" + red);
+        }
     }
 
     public void setGreen(int green) {
-        this.green = green;
+        if(green >= 0 && green <= 255){
+            this.green = green;
+        }else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setBlue(int blue) {
-        this.blue = blue;
+        if(blue >= 0 && blue <= 255){
+            this.blue = blue;
+        }else {
+            throw new IllegalArgumentException();
+        }
     }
 
+    public void setHexValue(String hexValue) {
+        String[] hexCode = hexValue.split("");
+        System.out.println(hexCode.length);
+        System.out.println(hexCode);
+        String hashtag = hexCode[0];
+        String[] red = Arrays.copyOfRange(hexCode,1,3);
+        String[] green = Arrays.copyOfRange(hexCode,3,5);
+        String[] blue = Arrays.copyOfRange(hexCode,5,7);
+
+//        String re = new String(red);
+//        System.out.println(re);
+//        System.out.println(red.length);
+//        System.out.println(Integer.parseInt(re,16));
+
+
+        HexValue = hexValue;
+    }
+
+
+    /**
+     * Getters
+     *
+     *
+     * **/
     public int getRed() {
         return red;
     }
@@ -40,13 +84,17 @@ public class Color {
         return blue;
     }
 
+    public String getHexValue() {
+        return HexValue;
+    }
+
     @Override
     public String toString() {
-        return "Color{" +
-                "red=" + red +
-                ", green=" + green +
-                ", blue=" + blue +
-                ", HexValue='" + HexValue + '\'' +
-                '}';
+        return "[" +
+                " Value=" + HexValue +
+                ", r=" + red +
+                ", g=" + green +
+                ", b=" + blue +
+                ']';
     }
 }
